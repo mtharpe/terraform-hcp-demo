e12b48718365075841c38ad5dc4783f85b53b02c
Michael Tharpe <mtharpe@hashicorp.com>
2021-09-18 16:49:38 -0400

    Oops

--- outputs.tf
+++ outputs.tf
@@ -3,9 +3,9 @@
 #########################################
 
 output "vault-public-address" {
-    value = hcp_vault_cluster.vault_cluster.public_endpoint_url
+    value = hcp_vault_cluster.vault_cluster.vault_public_endpoint_url
 }
 
 output "consul-public-address" {
-    value = hcp_consul_cluster.consul_cluster.public_endpoint_url
+    value = hcp_consul_cluster.consul_cluster.consul_public_endpoint_url
 }
