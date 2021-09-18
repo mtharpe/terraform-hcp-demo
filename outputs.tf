a38aed676b9d589a47b839ffac4947ab9801c4eb
Michael Tharpe <mtharpe@hashicorp.com>
2021-09-18 16:39:08 -0400

    Updated to just include the HCP config

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,3 @@
 #########################################
 # Specify outputs
 #########################################
-
-output "vault_token" {
-  value = hcp_vault_cluster_admin_token.vault_root_token.token
-  sensitive = true
-}
