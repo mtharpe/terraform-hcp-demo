5c47ad6e4dc6ddb48c61f77c365b0b8baa918e2b
Michael Tharpe <mtharpe@hashicorp.com>
2021-09-18 16:47:06 -0400

    Update to public endpoints with outputs

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,11 @@
 #########################################
 # Specify outputs
 #########################################
+
+output "vault-public-address" {
+    value = hcp_vault_cluster.vault_cluster.public_endpoint
+}
+
+output "consul-public-address" {
+    value = hcp_consul_cluster.consul_cluster.public_endpoint
+}
