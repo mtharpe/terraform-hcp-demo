530d9362fd961f8699b21a8108b9aff6dfb555c2
Michael Tharpe <mtharpe@hashicorp.com>
2021-09-18 15:33:10 -0400

    Updates with outputs

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
 #########################################
 # Specify outputs
 #########################################
+
+output "vault_token" {
+  value = hcp_vault_cluster_admin_token.vault_root_token.token
+}
