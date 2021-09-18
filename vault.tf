5b8e16b812f0c752508788c0523eddf664e0cea7
Michael Tharpe <mtharpe@hashicorp.com>
2021-09-18 15:28:06 -0400

    Update to configure Vault

--- vault.tf
+++ vault.tf
@@ -11,3 +11,21 @@ resource "hcp_vault_cluster" "vault_cluster" {
 resource "hcp_vault_cluster_admin_token" "vault_root_token" {
   cluster_id = "demo-vault-cluster"
 }
+
+#########################################
+# Specify the Vault Configuration
+#########################################
+
+provider "vault" {
+  namespace = "admin"
+  token     = hcp_vault_cluster_admin_token.vault_root_token.token
+}
+
+resource "vault_policy" "admin" {
+  name = "admin"
+
+  policy = <<EOT
+path "*" { capabilities = ["sudo","read","create","update","delete","list"] }
+}
+EOT
+}
