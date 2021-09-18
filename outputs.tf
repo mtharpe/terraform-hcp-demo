7cffa68329a93f2d8b1753875c90e3cddf866887
Michael Tharpe <mtharpe@hashicorp.com>
2021-09-18 16:48:53 -0400

    Add public to vault, and output the correct endpoint value

--- outputs.tf
+++ outputs.tf
@@ -3,9 +3,9 @@
 #########################################
 
 output "vault-public-address" {
-    value = hcp_vault_cluster.vault_cluster.public_endpoint
+    value = hcp_vault_cluster.vault_cluster.public_endpoint_url
 }
 
 output "consul-public-address" {
-    value = hcp_consul_cluster.consul_cluster.public_endpoint
+    value = hcp_consul_cluster.consul_cluster.public_endpoint_url
 }
--- vault.tf
+++ vault.tf
@@ -6,4 +6,5 @@ resource "hcp_vault_cluster" "vault_cluster" {
   cluster_id = "demo-vault-cluster"
   hvn_id     = hcp_hvn.hvn.hvn_id
   tier       = "standard_small"
+  public_endpoint = true
 }
