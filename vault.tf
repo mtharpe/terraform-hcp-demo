c81f3c15b8da41ebb81329b01cdb4409d1c6bbbb
Michael Tharpe <mtharpe@hashicorp.com>
2021-09-18 15:31:48 -0400

    Removed vault config, will have to run trigger it

--- vault.tf
+++ vault.tf
@@ -11,23 +11,3 @@ resource "hcp_vault_cluster" "vault_cluster" {
 resource "hcp_vault_cluster_admin_token" "vault_root_token" {
   cluster_id = "demo-vault-cluster"
 }
-
-#########################################
-# Specify the Vault Configuration
-#########################################
-
-provider "vault" {
-  address   = hcp_vault_cluster.vault_cluster.public_endpoint
-  token     = hcp_vault_cluster_admin_token.vault_root_token.token
-  namespace = "admin"
-
-}
-
-resource "vault_policy" "admin" {
-  name = "admin"
-
-  policy = <<EOT
-path "*" { capabilities = ["sudo","read","create","update","delete","list"] }
-}
-EOT
-}
